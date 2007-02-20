cb40d77fe9a7f489a44308e111c13d6d8f935a7c
Chusslove Illich <caslav.ilic@gmx.net>
2007-02-20 15:03:09 +0000

    i18n fixes. Note: %n is no longer used in plural strings.
    svn path=/trunk/KDE/kdesdk/kcachegrind/; revision=635655

--- kcachegrind/sourceitem.cpp
+++ kcachegrind/sourceitem.cpp
@@ -89,7 +89,7 @@ SourceItem::SourceItem(SourceView* sv, Q3ListViewItem* parent,
     callStr += i18n("Active call to '%1'",
                     _lineCall->call()->calledName());
   else
-    callStr += i18np("%1 call to '%2'", "%1 calls to '%2'", QString::number(cc),
+    callStr += i18np("%1 call to '%2'", "%1 calls to '%2'", cc,
                      _lineCall->call()->calledName());
 
   TraceFunction* calledF = _lineCall->call()->called();
