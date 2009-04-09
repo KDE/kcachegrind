4c364b0b6c6f431920bbb476d82e2f977c6c817a
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 20:08:10 +0000

    SVN_SILENT
    Updated Messages.sh to fetch strings from .ui and .rc files too.
    
    
    svn path=/trunk/KDE/kdesdk/kcachegrind/; revision=951630

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 (cd kcachegrind;$PREPARETIPS) > tips.cpp
+$EXTRACTRC `find kcachegrind -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT rc.cpp `find kcachegrind -name '*.cpp'` tips.cpp -o $podir/kcachegrind.pot
 $XGETTEXT_QT `find libcore -name '*.cpp'` `find libviews -name '*.cpp'` -o $podir/kcachegrind_qt.pot
 rm -f tips.cpp
