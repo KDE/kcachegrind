18c3464c3608f0aa63afac0c9b3a565e26afc49f
Laurent Montel <montel@kde.org>
2009-06-26 07:33:53 +0000

    Remove it too
    svn path=/trunk/KDE/kdesdk/kcachegrind/; revision=987478

--- Messages.sh
+++ Messages.sh
@@ -3,4 +3,4 @@
 $EXTRACTRC `find kcachegrind -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT rc.cpp `find kcachegrind -name '*.cpp'` tips.cpp -o $podir/kcachegrind.pot
 $XGETTEXT_QT `find libcore -name '*.cpp'` `find libviews -name '*.cpp'` -o $podir/kcachegrind_qt.pot
-rm -f tips.cpp
+rm -f tips.cpp rc.cpp
