2e71a3470b6e7ae8bb60b2fbfffa1000405bf872
Burkhard Lück <lueck@hube-lueck.de>
2009-10-07 14:30:21 +0000

    load the second translation catalog CCMAIL:Josef.Weidendorfer@gmx.de
    svn path=/trunk/KDE/kdesdk/kcachegrind/; revision=1032316

--- kcachegrind/main.cpp
+++ kcachegrind/main.cpp
@@ -55,6 +55,7 @@ int main( int argc, char ** argv )
   KCmdLineArgs::addCmdLineOptions( options );
 
   KApplication a;
+  KGlobal::locale()->insertCatalog("kcachegrind_qt");
   TopLevel* t;
   Loader::initLoaders();
 
