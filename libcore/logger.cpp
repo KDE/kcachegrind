a477883754431d4456e32cb7760870604c85ce37
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2015-08-12 11:36:59 +0200

    Update copyright years

--- libcore/logger.cpp
+++ libcore/logger.cpp
@@ -1,5 +1,5 @@
 /* This file is part of KCachegrind.
-   Copyright (C) 2008 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
+   Copyright (c) 2008-2015 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
 
    KCachegrind is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
--- libviews/traceitemview.cpp
+++ libviews/traceitemview.cpp
@@ -1,5 +1,5 @@
 /* This file is part of KCachegrind.
-   Copyright (C) 2003-2009 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
+   Copyright (c) 2003-2015 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
 
    KCachegrind is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
