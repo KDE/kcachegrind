6ad339284015e3bd818ae8d3f7c42cf1958202e8
Albert Astals Cid <aacid@kde.org>
2023-10-29 00:07:19 +0200

    Remove tip extraction now that KTipDialog is gone
    I'm leaving the tips file around so that the "tips should be moved to
    the documentation" part can be done
    
    CCMAIL: kde-doc-english@kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-(cd kcachegrind;$PREPARETIPS) > tips.cpp
 $EXTRACTRC `find kcachegrind -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT rc.cpp `find kcachegrind -name '*.cpp'` tips.cpp -o $podir/kcachegrind.pot
+$XGETTEXT rc.cpp `find kcachegrind -name '*.cpp'` -o $podir/kcachegrind.pot
 $EXTRACT_TR_STRINGS `find libcore -name '*.cpp'` `find libviews -name '*.cpp'` -o $podir/kcachegrind_qt.pot
-rm -f tips.cpp rc.cpp
+rm -f rc.cpp
